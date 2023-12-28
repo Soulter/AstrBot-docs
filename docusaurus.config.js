19b2603e031031d2235319c6ba363f7b67aae0f4
Soulter <905617992@qq.com>
2023-12-28 13:59:14 +0800

    feat: ignore broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'Soulter', // Usually your GitHub org/user name.
   projectName: 'AstrBot', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
